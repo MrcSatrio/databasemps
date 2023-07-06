8fc7d4419e4352b4d3907511daf947bdb02ce4a4
Marcellino Satrio Putro <121377213+MrcSatrio@users.noreply.github.com>
2023-07-06 15:31:54 +0700

    Add files via upload

--- db_mps.sql
+++ db_mps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 04, 2023 at 05:56 PM
+-- Generation Time: Jul 06, 2023 at 10:31 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -167,7 +167,7 @@ INSERT INTO `kartu` (`id_kartu`, `nomor_kartu`, `saldo`, `created_at`, `updated_
 (71, '0002576495', 40000, '2023-06-21 09:35:01', '2023-06-29 10:12:54'),
 (72, '8672762626', 100000, '2023-06-21 09:36:23', '2023-06-22 15:39:48'),
 (73, '0004909173', 40000, '2023-06-21 09:38:09', '2023-06-21 10:33:30'),
-(74, '', 170000, '2023-06-21 09:39:00', '2023-07-04 20:49:29'),
+(74, '00034462453', 210000, '2023-06-21 09:39:00', '2023-07-05 20:18:48'),
 (75, '45678987654', 240000, '2023-06-21 09:39:45', '2023-06-29 10:44:36'),
 (76, '0003704096', 87000, '2023-06-21 09:40:13', '2023-06-29 10:29:08'),
 (77, '0003707577', 17000, '2023-06-21 09:40:42', '2023-06-21 10:26:53'),
@@ -182,7 +182,8 @@ INSERT INTO `kartu` (`id_kartu`, `nomor_kartu`, `saldo`, `created_at`, `updated_
 (86, '87654567890', 20000, '2023-07-01 12:06:05', '2023-07-04 14:08:24'),
 (87, '', 20000, '2023-07-01 18:21:28', '2023-07-01 19:09:55'),
 (88, '0003446772', 20000, '2023-07-04 14:15:57', '2023-07-04 14:15:57'),
-(89, '00034436777', 20000, '2023-07-04 14:34:39', '2023-07-04 14:34:39');
+(89, '00034436777', 20000, '2023-07-04 14:34:39', '2023-07-04 14:34:39'),
+(90, '546454', 60000, '2023-07-05 20:15:01', '2023-07-06 09:36:45');
 
 -- --------------------------------------------------------
 
@@ -192,11 +193,11 @@ INSERT INTO `kartu` (`id_kartu`, `nomor_kartu`, `saldo`, `created_at`, `updated_
 
 CREATE TABLE `log_aktivitas` (
   `id_log` int(11) NOT NULL,
-  `npm` int(11) DEFAULT NULL,
+  `npm` int(11) NOT NULL,
   `ip_address` char(20) NOT NULL,
-  `action` varchar(255) DEFAULT NULL,
-  `details` varchar(255) DEFAULT NULL,
-  `timestamp` datetime DEFAULT current_timestamp()
+  `action` varchar(255) NOT NULL,
+  `details` varchar(255) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -204,73 +205,8 @@ CREATE TABLE `log_aktivitas` (
 --
 
 INSERT INTO `log_aktivitas` (`id_log`, `npm`, `ip_address`, `action`, `details`, `timestamp`) VALUES
-(13, 2, '110.138.91.228', 'login', 'User logged in', '2023-07-01 04:06:52'),
-(14, 2, '110.138.91.228', 'logout', 'User logged out', '2023-07-01 04:08:59'),
-(15, 2021320010, '52.14.196.50', 'login', 'User logged in', '2023-07-01 04:10:37'),
-(16, 1, '110.138.91.228', 'login', 'User logged in', '2023-07-01 04:11:49'),
-(17, 1, '110.138.91.228', 'logout', 'User logged out', '2023-07-01 04:12:04'),
-(18, 2021320004, '110.138.91.228', 'login', 'User logged in', '2023-07-01 04:12:09'),
-(19, 3, '110.138.91.228', 'login', 'User logged in', '2023-07-01 04:13:07'),
-(20, 2021320010, '110.138.91.228', 'login', 'User logged in', '2023-07-01 04:22:55'),
-(21, 2021320010, '110.138.91.228', 'topup', 'Topup dengan kode booking I8P64X sebesar 20000 berhasil dilakukan', '2023-07-01 04:23:03'),
-(22, 2021320010, '52.14.196.50', 'logout', 'User logged out', '2023-07-01 04:23:21'),
-(23, 2, '52.14.196.50', 'login', 'User logged in', '2023-07-01 04:23:25'),
-(24, 2021320010, '110.138.91.228', 'topup', 'Topup dengan kode booking 6FUB3S sebesar 20000', '2023-07-01 04:26:54'),
-(25, 2021320010, '110.138.91.228', 'transaksi_kartuHilang', 'Transaksi kartu hilang dengan kode booking SBJ4RX sebesar 50000', '2023-07-01 04:31:21'),
-(26, 2, '52.14.196.50', 'logout', 'User logged out', '2023-07-01 04:35:08'),
-(27, 1, '52.14.196.50', 'login', 'User logged in', '2023-07-01 04:35:15'),
-(28, 1, '52.14.196.50', 'transaksi_approve', 'Transaksi dengan kode booking SBJ4RX telah disetujui', '2023-07-01 04:35:27'),
-(29, 2021320010, '110.138.91.228', 'transaksi_Topup', 'Topup dengan kode booking TCRPP7 sebesar 20000', '2023-07-01 04:38:59'),
-(30, 2021320010, '110.138.91.228', 'Transaksi_Cancel', 'Transaksi dengan ID 193 dibatalkan', '2023-07-01 04:39:04'),
-(31, 2021320010, '110.138.91.228', 'transaksi_Topup', 'Topup dengan kode booking 3LW7OR sebesar 20000', '2023-07-01 04:43:23'),
-(32, 1, '52.14.196.50', 'logout', 'User logged out', '2023-07-01 04:43:34'),
-(33, 2, '52.14.196.50', 'login', 'User logged in', '2023-07-01 04:43:48'),
-(34, 2, '52.14.196.50', 'transaksi_approve', 'Transaksi dengan kode booking 3LW7OR disetujui', '2023-07-01 04:44:08'),
-(35, 2, '52.14.196.50', 'logout', 'User logged out', '2023-07-01 04:49:47'),
-(36, 1, '52.14.196.50', 'login', 'User logged in', '2023-07-01 04:49:52'),
-(39, 1, '', 'User_Delete', 'Pengguna 2021320032Telah Dihapus', '2023-07-01 04:57:47'),
-(40, 2021320010, '110.138.91.228', 'logout', 'User logged out', '2023-07-01 05:02:39'),
-(41, 1, '110.138.91.228', 'login', 'User logged in', '2023-07-01 05:02:45'),
-(42, 1, '', 'User Insertion', 'Pengguna 2021320019 Ditambahkan', '2023-07-01 05:03:06'),
-(43, 1, '', 'User_Delete', 'Pengguna 2021320019 Telah Dihapus', '2023-07-01 05:04:13'),
-(44, 2, '52.14.196.50', 'login', 'User logged in', '2023-07-01 05:05:45'),
-(45, 2, '', 'User_Add', 'Pengguna 2021320019 Ditambahkan', '2023-07-01 05:06:05'),
-(46, 1, '52.14.196.50', 'User_Update', 'Pengguna 2021320006 Telah Diperbarui', '2023-07-01 05:12:05'),
-(47, 2, '140.213.35.119', 'login', 'User logged in', '2023-07-01 09:58:31'),
-(48, 2, '120.188.6.198', 'login', 'User logged in', '2023-07-01 11:20:04'),
-(49, 2, '120.188.6.198', 'User_Add', 'Pengguna 2021320109 Ditambahkan', '2023-07-01 11:21:28'),
-(50, 2, '120.188.6.198', 'logout', 'User logged out', '2023-07-01 11:21:34'),
-(51, NULL, '120.188.6.198', 'logout', 'User logged out', '2023-07-01 11:21:35'),
-(52, NULL, '120.188.6.198', 'logout', 'User logged out', '2023-07-01 11:21:50'),
-(53, 2021320109, '120.188.6.198', 'login', 'User logged in', '2023-07-01 11:22:00'),
-(54, 2021320109, '120.188.6.198', 'login', 'User logged in', '2023-07-01 12:09:08'),
-(55, 2021320109, '120.188.6.198', 'transaksi_KartuHilang', 'Transaksi kartu hilang dengan kode booking H10D0F sebesar 0', '2023-07-01 12:09:55'),
-(56, 2021320109, '120.188.6.198', 'logout', 'User logged out', '2023-07-01 12:13:42'),
-(57, 2021320010, '110.138.91.228', 'login', 'User logged in', '2023-07-01 14:17:00'),
-(58, 2021320004, '118.99.106.212', 'transaksi_Topup', 'Topup dengan kode booking AZHNZZ sebesar 20000', '2023-07-04 05:42:26'),
-(59, 2021320004, '118.99.106.212', 'Transaksi_Cancel', 'Transaksi dengan ID 196 dibatalkan', '2023-07-04 05:43:15'),
-(60, 1, '118.99.106.212', 'User_Update', 'Pengguna 2021320004 Telah Diperbarui', '2023-07-04 06:01:07'),
-(61, 2021320004, '118.99.106.212', 'transaksi_Topup', 'Topup dengan kode booking R3HW3V sebesar 50000', '2023-07-04 06:42:41'),
-(62, 2, '118.99.106.212', 'transaksi_approve', 'Transaksi dengan kode booking R3HW3V disetujui', '2023-07-04 06:52:44'),
-(63, 1, '118.99.106.212', 'User_Update', 'Pengguna 2021320019 Telah Diperbarui', '2023-07-04 07:08:24'),
-(64, 1, '118.99.106.212', 'User_Add', 'Pengguna 2021320123 Ditambahkan', '2023-07-04 07:15:57'),
-(65, 2021320004, '118.99.106.212', 'transaksi_Topup', 'Topup dengan kode booking BJWO7R sebesar 50000', '2023-07-04 07:21:14'),
-(66, 1, '118.99.106.212', 'transaksi_approve', 'Transaksi dengan kode booking BJWO7R disetujui', '2023-07-04 07:22:40'),
-(67, 2, '118.99.106.212', 'User_Add', 'Pengguna 2021320063 Ditambahkan', '2023-07-04 07:34:39'),
-(68, 2021320004, '118.99.106.212', 'transaksi_KartuHilang', 'Transaksi kartu hilang dengan kode booking 5QJ7YT sebesar 50000', '2023-07-04 07:44:10'),
-(69, 2021320004, '118.99.106.212', 'Transaksi_Cancel', 'Transaksi dengan ID 199 dibatalkan', '2023-07-04 08:13:45'),
-(70, 2021320004, '118.99.106.212', 'transaksi_KartuHilang', 'Transaksi kartu hilang dengan kode booking EZDKFE sebesar 0', '2023-07-04 08:14:05'),
-(71, 2, '118.99.106.212', 'transaksi_approve', 'Transaksi dengan kode booking EZDKFE disetujui', '2023-07-04 08:15:40'),
-(72, 2021320004, '118.99.106.212', 'transaksi_KartuHilang', 'Transaksi kartu hilang dengan kode booking BE5X9P sebesar 0', '2023-07-04 08:16:37'),
-(73, 2, '118.99.106.212', 'transaksi_approve', 'Transaksi dengan kode booking BE5X9P disetujui', '2023-07-04 08:17:21'),
-(74, 2021320004, '110.138.92.75', 'transaksi_Topup', 'Topup dengan kode booking W1J6G3 sebesar 20000', '2023-07-04 13:46:44'),
-(75, 2021320004, '110.138.92.75', 'Transaksi_Cancel', 'Transaksi dengan ID 202 dibatalkan', '2023-07-04 13:47:36'),
-(76, 2021320004, '110.138.92.75', 'transaksi_Topup', 'Topup dengan kode booking GUKBQ6 sebesar 20000', '2023-07-04 13:47:47'),
-(77, 2021320004, '110.138.92.75', 'Transaksi_Cancel', 'Transaksi dengan ID 203 dibatalkan', '2023-07-04 13:48:12'),
-(78, 2021320004, '110.138.92.75', 'transaksi_Topup', 'Topup dengan kode booking 7W158K sebesar 20000', '2023-07-04 13:48:21'),
-(79, 2021320004, '110.138.92.75', 'Transaksi_Cancel', 'Transaksi dengan ID 204 dibatalkan', '2023-07-04 13:48:39'),
-(80, 2021320004, '110.138.92.75', 'transaksi_KartuHilang', 'Transaksi kartu hilang dengan kode booking RJVML9 sebesar 0', '2023-07-04 13:49:29'),
-(81, 2021320004, '110.138.92.75', 'Transaksi_Cancel', 'Transaksi dengan ID 205 dibatalkan', '2023-07-04 13:50:11');
+(1, 1, '110.138.80.26', 'User_Delete', 'Pengguna 2021320004 Telah Dihapus', '2023-07-06 08:30:16'),
+(2, 1, '110.138.80.26', 'User_Delete', 'Pengguna 2021320063 Telah Dihapus', '2023-07-06 08:30:26');
 
 -- --------------------------------------------------------
 
@@ -402,7 +338,14 @@ INSERT INTO `transaksi` (`id_transaksi`, `kodebooking_transaksi`, `npm`, `id_jen
 (202, 'W1J6G3', 2021320004, 1, 170000, 20000, 190000, 1, 4, '', '2023-07-04 20:46:44', '2023-07-04 20:47:36', ''),
 (203, 'GUKBQ6', 2021320004, 1, 170000, 20000, 190000, 2, 4, '', '2023-07-04 20:47:47', '2023-07-04 20:48:12', ''),
 (204, '7W158K', 2021320004, 1, 170000, 20000, 190000, 1, 4, '', '2023-07-04 20:48:21', '2023-07-04 20:48:39', ''),
-(205, 'RJVML9', 2021320004, 2, 170000, 0, 170000, 1, 4, '', '2023-07-04 20:49:29', '2023-07-04 20:50:11', '');
+(205, 'RJVML9', 2021320004, 2, 170000, 0, 170000, 1, 4, '', '2023-07-04 20:49:29', '2023-07-04 20:50:11', ''),
+(206, 'JH0ZUP', 2021320004, 1, 170000, 20000, 190000, 1, 3, '', '2023-07-05 20:11:47', '2023-07-05 20:16:02', 'arief fadhil'),
+(207, 'ICL2GM', 2021320004, 1, 190000, 20000, 210000, 2, 3, '', '2023-07-05 20:16:18', '2023-07-05 20:16:27', 'arief fadhil'),
+(208, 'ME8CM6', 2021320004, 2, 210000, 0, 210000, 2, 3, '20230705201703_1688563023_0a4c40700945be4b6810.png', '2023-07-05 20:16:44', '2023-07-05 20:18:48', 'arief fadhil'),
+(209, 'B43MOY', 2021320004, 1, 210000, 20000, 230000, 1, 4, '', '2023-07-05 20:18:59', '2023-07-05 20:19:04', ''),
+(210, 'AYG2PD', 2021320063, 1, 20000, 20000, 40000, 1, 3, '', '2023-07-05 20:49:57', '2023-07-05 20:51:08', 'arief fadhil'),
+(211, 'VSIEWW', 2021320063, 2, 40000, 0, 40000, 1, 4, '', '2023-07-05 20:52:22', '2023-07-05 20:53:27', ''),
+(212, 'GWKTSQ', 2021320063, 1, 40000, 20000, 60000, 2, 3, '20230705205446_1688565286_9cbe14c4a3d69af94e93.png', '2023-07-05 20:53:44', '2023-07-06 09:36:45', 'Admin Root');
 
 -- --------------------------------------------------------
 
@@ -432,16 +375,8 @@ INSERT INTO `user` (`npm`, `id_kartu`, `id_role`, `id_status`, `masa_berlaku`, `
 (1, 3, 1, 1, '0000-00-00 00:00:00', 'Admin Root', 'rahmantoadmojo@gmail.com', 'e64b78fc3bc91bcbc7dc232ba8ec59e0', '', '2023-05-05 03:55:40', '2023-06-29 00:11:10'),
 (2, 4, 2, 2, '0000-00-00 00:00:00', 'arief fadhil', 'Arieffdhl@gmail.com', '202cb962ac59075b964b07152d234b70', '993554', '2023-05-05 03:57:11', '2023-07-01 10:54:13'),
 (3, 5, 3, 1, '0000-00-00 00:00:00', 'dapidah', 'davidst@gmail.com', '202cb962ac59075b964b07152d234b70', '', '2023-05-05 03:58:02', '2023-06-16 01:37:25'),
-(2021320004, 74, 4, 1, '0000-00-00 00:00:00', 'User1', 'yyvcc88@gmail.com', '0406a18e88e08146adcd5615610d80f1', '774860', '2023-06-21 09:39:00', '2023-07-04 14:50:31'),
-(2021320006, 79, 4, 1, '0000-00-00 00:00:00', 'Atika Hanifah Cantik', 'atikahanifah35@gmail.com', '3110147156938d8288f99d2233dd5ae2', '', '2023-06-21 09:41:47', '2023-07-01 12:12:05'),
 (2021320010, 84, 4, 1, '0000-00-00 00:00:00', 'Marcellino Satrio Putro', 'Marcsatrioff@gmail.com', '1809c4a1ed69581a8e082b76046de7c0', '', '2023-07-01 10:57:23', '2023-07-01 10:58:20'),
 (2021320019, 86, 4, 1, '0000-00-00 00:00:00', 'Marcellino', 'marcsatrio2222ff@gmail.com', '433f0d6073c60045a6568ded1c2559ff', '', '2023-07-01 12:06:05', '2023-07-04 14:08:24'),
-(2021320021, 77, 4, 1, '0000-00-00 00:00:00', 'Agustin Amalia Saputri', 'agustinamaliasaputri@gmail.com', 'c55623f9dfb97a64a7c7346d71fc863b', '', '2023-06-21 09:40:42', '2023-06-21 09:40:42'),
-(2021320042, 73, 4, 2, '2023-06-22 00:00:00', 'Faradila Galuh Pramesti', 'faradilapramesti5181@gmail.com', '0ec5752fc999a6ab2b7bc866848ad69b', '', '2023-06-21 09:38:09', '2023-06-21 10:33:29'),
-(2021320063, 89, 4, 1, '0000-00-00 00:00:00', 'Alexa Sellena', 'Alexa11@gmail.com', '0e0931a27e7d64557725cbe098803825', '', '2023-07-04 14:34:39', '2023-07-04 14:34:39'),
-(2021320085, 83, 4, 1, '0000-00-00 00:00:00', 'Jhoni Susanto', 'j.susanto103@gmail.com', 'a7d62590f3784d8c12e698da06c3ab92', '', '2023-06-21 09:53:43', '2023-06-21 10:19:15'),
-(2021320090, 81, 4, 2, '2023-07-21 00:00:00', 'Mauhamad Raihan Fawwaz Ramadhan', 'muhamadraihan1511@gmail.com', '5fbeb23148308e3d5b626655fea8da9c', '', '2023-06-21 09:44:31', '2023-06-21 09:49:09'),
-(2021320108, 72, 4, 1, '0000-00-00 00:00:00', 'Nahdah Rizky Nur Afifah', 'ernahdah@gmail.com', '3ef7ab4d6db7968b2af086ea45706e48', '', '2023-06-21 09:36:23', '2023-06-21 09:36:23'),
 (2021320109, 87, 4, 1, '0000-00-00 00:00:00', 'Arief Fadhillah', 'aeferforbiu@gmail.com', '5e97ea322d7e189cfdc2c983ca7959f8', '', '2023-07-01 18:21:28', '2023-07-01 18:21:28'),
 (2021320123, 88, 4, 1, '0000-00-00 00:00:00', 'Jackson Marino', 'jack1010@gmail.com', '0872ce176670a9f639e566127999f683', '', '2023-07-04 14:15:57', '2023-07-04 14:15:57');
 
@@ -483,8 +418,7 @@ ALTER TABLE `kartu`
 -- Indexes for table `log_aktivitas`
 --
 ALTER TABLE `log_aktivitas`
-  ADD PRIMARY KEY (`id_log`),
-  ADD KEY `npm` (`npm`);
+  ADD PRIMARY KEY (`id_log`);
 
 --
 -- Indexes for table `role`
@@ -543,29 +477,23 @@ ALTER TABLE `harga`
 -- AUTO_INCREMENT for table `kartu`
 --
 ALTER TABLE `kartu`
-  MODIFY `id_kartu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `id_kartu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
 
 --
 -- AUTO_INCREMENT for table `log_aktivitas`
 --
 ALTER TABLE `log_aktivitas`
-  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=206;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=213;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `log_aktivitas`
---
-ALTER TABLE `log_aktivitas`
-  ADD CONSTRAINT `log_aktivitas_ibfk_1` FOREIGN KEY (`npm`) REFERENCES `user` (`npm`);
 
 --
 -- Constraints for table `transaksi`
